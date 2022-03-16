278acb2584271f08885239a4527e37944e08aab4
Jerome Bidault <djedje.thai.ok@gmail.com>
2022-03-16 14:12:35 +0700

    gothrought the app done

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ go build -o vigilate cmd/web/*.go && ./vigilate \
 -pusherHost='localhost' \
 -pusherKey='abc123' \
 -pusherSecret='123abc' \
--pusherApp="1"
-#-pusherPort="4001"
-#-pusherSecure=false
+-pusherApp="1" \
+-pusherPort="4001" \
+-pusherSecure=false
