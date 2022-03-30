4218dbce1f019e3d7235ad414841c007d0375615
Jerome Bidault <djedje.thai.ok@gmail.com>
2022-03-30 08:49:52 +0700

    test service implemented

--- internal/handlers/perform-checks.go
+++ internal/handlers/perform-checks.go
@@ -1,17 +1,116 @@
 package handlers
 
 import (
-	"github.com/go-chi/chi"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
+
+	"github.com/djedjethai/vigilate/internal/models"
+	"github.com/go-chi/chi"
 )
 
-func (m *DBRepo) TestCheck(w http.ResponseWriter, r *http.Request) {
+const (
+	STARTIOTA = iota
+	HTTP
+	HTTPS
+	SSLCertificate
+)
+
+type jsonResp struct {
+	OK            bool      `json:"ok"`
+	Message       string    `json:"message"`
+	ServiceID     int       `json:"service_id"`
+	HostServiceID int       `json:"host_service_id"`
+	HostID        int       `json:"host_id"`
+	OldStatus     string    `json:"old_status"`
+	NewStatus     string    `json:"new_status"`
+	LastCheck     time.Time `json:"last_check"`
+}
+
+func (repo *DBRepo) TestCheck(w http.ResponseWriter, r *http.Request) {
 	hostServiceID, _ := strconv.Atoi(chi.URLParam(r, "id"))
 	oldStatus := chi.URLParam(r, "oldStatus")
+	okay := true
 
-	log.Println(hostServiceID, oldStatus)
+	// get host service
+	hs, err := repo.DB.GetHostServiceByID(hostServiceID)
+	if err != nil {
+		okay = false
+		log.Println(err)
+	}
 
-	//
+	// get host?
+	h, err := repo.DB.GetHostByID(hs.HostID)
+	if err != nil {
+		okay = false
+		log.Println(err)
+	}
+
+	// test the service
+	msg, newStatus := repo.testServiceForHost(h, hs)
+
+	// update the host service in the database with status(if changed) and last check
+
+	// broadcast service status changed event
+
+	var resp jsonResp
+
+	if okay {
+		resp = jsonResp{
+			OK:            okay,
+			Message:       msg,
+			ServiceID:     hs.ServiceID,
+			HostServiceID: hs.ID,
+			HostID:        hs.HostID,
+			OldStatus:     oldStatus,
+			NewStatus:     newStatus,
+			LastCheck:     time.Now(),
+		}
+	} else {
+		resp.OK = false
+		resp.Message = "Something went wrong"
+	}
+
+	out, _ := json.MarshalIndent(resp, "", "   ")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+func (repo *DBRepo) testServiceForHost(h models.Host, hs models.HostService) (string, string) {
+
+	var msg, newStatus string
+
+	switch hs.ServiceID {
+	case HTTP:
+		fmt.Println("this is case http")
+		msg, newStatus = testHTTPForHost(h.URL)
+		break
+	}
+
+	return msg, newStatus
+}
+
+func testHTTPForHost(url string) (string, string) {
+	if strings.HasSuffix(url, "/") {
+		strings.TrimSuffix(url, "/")
+	}
+
+	url = strings.Replace(url, "https://", "http://", -1)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("in the errrr")
+		return fmt.Sprintf("%s - %s", url, "error connecting"), "problem"
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Sprintf("%s - %s", url, resp.Status), "problem"
+	}
+
+	return fmt.Sprintf("%s - %s", url, resp.Status), "healthy"
 }
